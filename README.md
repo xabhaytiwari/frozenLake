b34218e2d576adacc8313702d98542e455c44855
Abhay Tiwari <emailatabhay@gmail.com>
2025-07-23 09:51:37 +0000

    Solved from scratch Q, FrozenLake-v1, Taxi-v3

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Solved FrozenLake-v1 using Q-Learning from scratch! 
+Solved FrozenLake-v1 and Taxi-v3 using Q-Learning from scratch! 
--- main.py
+++ main.py
@@ -122,7 +122,43 @@ def evaluate_agent(env, max_steps, n_eval_episodes, Q, seed):
   return mean_reward, std_reward
 
 # Evaluate our Agent
-mean_reward, std_reward = evaluate_agent(env, max_steps, n_eval_episodes, Qtable_frozenlake, eval_seed)
-print(f"Mean_reward={mean_reward:.2f} +/- {std_reward:.2f}")
+mean_reward_frozenLake, std_reward_frozenLake = evaluate_agent(env, max_steps, n_eval_episodes, Qtable_frozenlake, eval_seed)
+print(f"Mean_reward={mean_reward_frozenLake:.2f} +/- {std_reward_frozenLake:.2f}")
 
-# Solve Taxi-v3 ~
+# Solve Taxi-v3
+env = gym.make("Taxi-v3", render_mode="rgb_array")
+
+state_space = env.observation_space.n
+action_space = env.action_space.n
+
+Qtable_taxi = initialise_q_table(state_space, action_space)
+print(Qtable_taxi)
+
+# Taxi - v3 Hyperparemeters
+# Training parameters
+n_training_episodes = 25000   # Total training episodes
+learning_rate = 0.7           # Learning rate
+
+# Evaluation parameters
+n_eval_episodes = 100        # Total number of test episodes
+
+# DO NOT MODIFY EVAL_SEED
+eval_seed = [16,54,165,177,191,191,120,80,149,178,48,38,6,125,174,73,50,172,100,148,146,6,25,40,68,148,49,167,9,97,164,176,61,7,54,55,
+ 161,131,184,51,170,12,120,113,95,126,51,98,36,135,54,82,45,95,89,59,95,124,9,113,58,85,51,134,121,169,105,21,30,11,50,65,12,43,82,145,152,97,106,55,31,85,38,
+ 112,102,168,123,97,21,83,158,26,80,63,5,81,32,11,28,148] # Evaluation seed, Each seed has a specific starting state
+
+# Environment parameters
+env_id = "Taxi-v3"           # Name of the environment
+max_steps = 99               # Max steps per episode
+gamma = 0.95                 # Discounting rate
+
+# Exploration parameters
+max_epsilon = 1.0             # Exploration probability at start
+min_epsilon = 0.05           # Minimum exploration probability
+decay_rate = 0.005            # Exponential decay rate for exploration prob
+
+Qtable_taxi = train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable_taxi)
+print(Qtable_taxi)
+
+mean_reward_taxi, std_reward_taxi = evaluate_agent(env, max_steps, n_eval_episodes, Qtable_taxi, eval_seed)
+print(f"Mean_reward={mean_reward_taxi:.2f} +/- {std_reward_taxi:.2f}")
